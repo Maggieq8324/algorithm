b6e0b660039c6dc08f187e880fbd4a240f0ffff8
李通 <maggieq8324@gmail.com>
2021-07-08 15:46:05 +0800

    add LeetCode 最小栈

--- src/leetCode/MinStack.java
+++ src/leetCode/MinStack.java
@@ -1,5 +1,7 @@
 package leetCode;
 
+import java.util.Stack;
+
 /**
  * @Description LeetCode 155. 最小栈
  * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
@@ -16,26 +18,76 @@ package leetCode;
  */
 public class MinStack {
 
-    /** initialize your data structure here. */
+    /**
+     * 栈
+     */
+    private Stack<Integer> stack;
+
+    /**
+     * 辅助栈
+     */
+    private Stack<Integer> minStack;
+
+    /**
+     * 初始化
+     */
     public MinStack() {
-
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
+    /**
+     * 将元素推入栈中
+     * 每当push()新值进来时，
+     * 如果小于等于 minStack，
+     * 则一起push()到minStack，即更新了栈顶最小值
+     * @param val
+     */
     public void push(int val) {
+        stack.add(val);
 
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.add(val);
+        }
     }
 
+    /**
+     * 删除栈顶的元素
+     * 判断将pop()出去的元素值是否是minStack栈顶元素值（即最小值），
+     * 如果是则将minStack栈顶元素一起pop()，
+     * 这样可以保证minStack栈顶元素始终是stack中的最小值。
+     */
     public void pop() {
-
+        if (!stack.isEmpty()) {
+            if (stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+        }
     }
 
+    /**
+     * 获取栈顶元素
+     * @return
+     */
     public int top() {
-        return 1;
+        if (!stack.isEmpty()) {
+            return stack.peek();
+        }
+        throw new RuntimeException("栈中元素为空，此操作非法");
     }
 
+    /**
+     * 检索栈中的最小元素
+     * 返回minStack栈顶
+     * @return
+     */
     public int getMin() {
-        return 1;
+        if (!minStack.isEmpty()) {
+            return minStack.peek();
+        }
+        throw new RuntimeException("栈中元素为空，此操作非法");
     }
+
 }
 
 /**
